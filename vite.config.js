dd03478ee160f9f6ad3f8476e508d88616886df9
Andrew <andrewcalabrese86@gmail.com>
2026-02-25 08:46:31 -0500

    Remove ios-app; make project web-only

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: 'ios-app',
+  root: '.',
   build: {
     target: 'esnext',
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: resolve(__dirname, 'ios-app/index.html'),
-      external: ['@capawesome/capacitor-file-picker'],
+      input: resolve(__dirname, 'index.html'),
     },
   },
   server: {
